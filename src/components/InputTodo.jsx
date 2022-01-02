42df53d400e44a904d14e9e56513da7ee124f9a1
mocchi-github <96935945+mocchi-github@users.noreply.github.com>
2022-01-03 00:07:49 +0900

    InputTodo固有のスタイルをInputTodoクラス内で定義

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+const inputAreaStyle = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 const InputTodo = (props) => {
   const { text, onChange, onClick } = props;
 
   return (
-    <div className="input-area">
+    <div style={inputAreaStyle}>
       <input placeholder="TODOを入力" value={text} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
