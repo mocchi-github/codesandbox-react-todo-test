b77298a5a47696357727e4351ae304d8a152b6a2
mocchi-github <96935945+mocchi-github@users.noreply.github.com>
2022-01-02 23:38:40 +0900

    TODO追加・削除処理

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,53 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["aaa", "bbb"]);
   const [completeTodos, setCompleteTodos] = useState(["done1", "done2"]);
+
+  const onChangeTodoText = (evevt) => {
+    setTodoText(evevt.target.value);
+  };
+  const onClickAddButton = () => {
+    if (!todoText) {
+      return;
+    }
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+  const onClickDeleteButton = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAddButton}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, idx) => {
             return (
               <li key={todo}>
                 <div className="list-row">
                   <span>{todo}</span>
                   <button>完了</button>
-                  <button>削除</button>
+                  <button
+                    onClick={() => {
+                      onClickDeleteButton(idx);
+                    }}
+                  >
+                    削除
+                  </button>
                 </div>
               </li>
             );
